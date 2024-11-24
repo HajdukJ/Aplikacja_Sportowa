e250fc377dce20dfc2652ecdd15096177f912355
HomoncikS <seweryn.h@opoczta.pl>
2024-11-24 21:17:31 +0100

    Dodanie layoutu dla RunFragment

--- app/src/main/java/com/example/aplikacja_sportowa/RunFragment.kt
+++ app/src/main/java/com/example/aplikacja_sportowa/RunFragment.kt
@@ -1,3 +1,4 @@
+
 package com.example.aplikacja_sportowa
 
 import android.os.Bundle
@@ -5,12 +6,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 
 class RunFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_run, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_run, container, false)
+
+        val runningButton = view.findViewById<Button>(R.id.runningButton)
+        val cyclingButton = view.findViewById<Button>(R.id.cyclingButton)
+
+        runningButton.setOnClickListener {
+            Toast.makeText(context, "Current activity: Running", Toast.LENGTH_SHORT).show()
+        }
+
+        cyclingButton.setOnClickListener {
+            Toast.makeText(context, "Current activity: Cycling", Toast.LENGTH_SHORT).show()
+        }
+
+        return view
     }
 }
